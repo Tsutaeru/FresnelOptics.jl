d1b9caa27f32a8c78190920cc80173cc5be918b2
yctai <brianyjtai1994@gmail.com>
2021-07-23 23:09:39 +0800

    Add: Aliases of abstract types.

--- src/FresnelOptics.jl
+++ src/FresnelOptics.jl
@@ -1,5 +1,14 @@
 module FresnelOptics
 
-greet() = print("Hello World!")
+const VecI  = AbstractVector # Input  Vector
+const VecO  = AbstractVector # Output Vector
+const VecB  = AbstractVector # Buffer Vector
+const VecIO = AbstractVector # In/Out Vector
+const MatI  = AbstractMatrix # Input  Matrix
+const MatO  = AbstractMatrix # Output Matrix
+const MatB  = AbstractMatrix # Buffer Matrix
+const MatIO = AbstractMatrix # In/Out Matrix
+
+const 𝚷 = 2.0 * π
 
 end # module
